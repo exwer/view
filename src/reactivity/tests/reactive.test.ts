f1c0d61d1af34c6f4cc300ff34c27d44de762b5c
lbw <574524020@qq.com>
2022-04-29 16:15:53 +0800

    feat:通过reactive的happy path

--- src/reactivity/tests/reactive.test.ts
+++ src/reactivity/tests/reactive.test.ts
@@ -1,9 +1,11 @@
-import { describe, expect } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { reactive } from '../index'
 
 describe('reactive', () => {
-  const original = { foo: 1 }
-  const observed = reactive(original)
-  expect(observed).not.toBe(original)
-  expect(observed.foo).toBe(1)
+  it('happy path', () => {
+    const original = { foo: 1 }
+    const observed = reactive(original)
+    expect(observed).not.toBe(original)
+    expect(observed.foo).toBe(1)
+  })
 })
