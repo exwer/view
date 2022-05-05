ee546dd9a209e6b7607c914f95e6c1d277b35f87
lbw <574524020@qq.com>
2022-05-05 18:31:43 +0800

    feat:track

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,16 +1,36 @@
+let activeEffect: ReactiveEffect
 class ReactiveEffect {
   private _fn: any
-  constructor(fn) {
+  constructor(fn: Function) {
     this._fn = fn
   }
 
   run() {
+    activeEffect = this
     this._fn()
   }
 }
 
-export function effect(fn) {
+export function effect(fn: Function) {
   const _effect = new ReactiveEffect(fn)
 
   _effect.run()
+}
+
+const targetMap = new Map()
+export function track(target: Record<any, any>, key: any) {
+  // target:key -> dep
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    depsMap = new Map()
+    targetMap.set(target, depsMap)
+  }
+
+  let dep = depsMap.get(key)
+  if (!dep) {
+    dep = new Set()
+    depsMap.set(key, dep)
+  }
+
+  dep.add(activeEffect)
 }
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,9 +1,12 @@
-export function reactive(raw) {
+import { track } from './effect'
+
+export function reactive(raw: Record<any, any>) {
   return new Proxy(raw, {
     get(target, key) {
       const res = Reflect.get(target, key)
 
       // TODO 依赖收集
+      track(target, key)
       return res
     },
     set(target, key, value) {
