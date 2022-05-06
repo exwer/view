c315d12bceecb02c21122cbba20ac04251d1124a
lbw <574524020@qq.com>
2022-05-06 16:54:24 +0800

    feat:runner

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -11,7 +11,7 @@ class ReactiveEffect {
 
   run() {
     activeEffect = this
-    this._fn()
+    return this._fn()
   }
 }
 
@@ -19,6 +19,8 @@ export function effect(fn: Function) {
   const _effect = new ReactiveEffect(fn)
 
   _effect.run()
+
+  return _effect.run.bind(_effect)
 }
 
 export function track(target: Target, key: any) {
