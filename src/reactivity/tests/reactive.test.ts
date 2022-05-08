0f4c0e0e8fe35a9a030b2dbc8ebf8613a66a8770
John Walker <ss.lbw.ss@gmail.com>
2022-05-08 23:24:36 +0800

    refactor:seperate readonly test

--- src/reactivity/tests/reactive.test.ts
+++ src/reactivity/tests/reactive.test.ts
@@ -5,19 +5,15 @@ describe('reactive', () => {
   it('happy path', () => {
     const original = { foo: 1 }
     const observed = reactive(original)
-    const constant = readonly(original)
     expect(observed).not.toBe(original)
     expect(observed.foo).toBe(1)
 
     expect(isReactive(observed)).toBe(true)
     expect(isReactive(original)).toBe(false)
-    expect(isReadonly(constant)).toBe(true)
-    expect(isReadonly(original)).toBe(false)
     const evil = {
       __v_isReactive: true,
       __v_isReadonly: true,
     }
     expect(isReactive(evil)).toBe(false)
-    expect(isReadonly(evil)).toBe(false)
   })
 })
