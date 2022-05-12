cce10f80e6c3849d610513843ca4b3312a1845ce
lbw <574524020@qq.com>
2022-05-12 19:26:43 +0800

    test: ProxyRef

--- src/reactivity/tests/ref.test.ts
+++ src/reactivity/tests/ref.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from 'vitest'
-import { isRef, ref, unRef } from '../ref'
+import { isRef, proxyRefs, ref, unRef } from '../ref'
 import { effect } from '../effect'
 import { reactive } from '../reactive'
 
@@ -57,6 +57,22 @@ describe('ref', () => {
     const a = ref(1)
     expect(unRef(a)).toBe(1)
     expect(unRef(1)).toBe(1)
+  })
+
+  it('proxyRefs', () => {
+    // used in template
+    const user = {
+      age: ref(10),
+      name: 'ming',
+    }
+    const proxyUser = proxyRefs(user)
+    expect(user.age.value).toBe(10)
+    expect(proxyUser.age).toBe(10)
+    expect(proxyUser.name).toBe('ming')
+
+    proxyUser.age = 20
+    expect(proxyUser.age).toBe(20)
+    expect(user.age.value).toBe(20)
   })
 })
 
