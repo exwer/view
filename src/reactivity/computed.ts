1cdf50baf78df508362dacd70abdfaeda0e17e3b
lbw <574524020@qq.com>
2022-05-13 10:57:54 +0800

    feat:computed dirty

--- src/reactivity/computed.ts
+++ src/reactivity/computed.ts
@@ -1,11 +1,17 @@
 class ComputedRefImpl {
   private _getter: any
+  private _dirty = true
+  private _value: any
   constructor(getter: any) {
     this._getter = getter
   }
 
   get value() {
-    return this._getter()
+    if (this._dirty) {
+      this._dirty = false
+      this._value = this._getter()
+    }
+    return this._value
   }
 }
 
--- src/reactivity/tests/computed.test.ts
+++ src/reactivity/tests/computed.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
 import { computed } from '../computed'
 import { reactive } from '../reactive'
 
@@ -10,5 +10,25 @@ describe('computed', () => {
 
     const age = computed(() => user.age)
     expect(age.value).toBe(1)
+  })
+
+  it('should compute lazily', () => {
+    const value = reactive({
+      foo: 1,
+    })
+    const getter = vi.fn(() => {
+      return value.foo
+    })
+    const cValue = computed(getter)
+
+    // lazy
+    expect(getter).not.toHaveBeenCalled()
+
+    expect(cValue.value).toBe(1)
+    expect(getter).toHaveBeenCalledTimes(1)
+
+    // should not compute again
+    cValue.value
+    expect(getter).toHaveBeenCalledTimes(1)
   })
 })
