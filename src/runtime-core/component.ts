cb8bcf66643a2b291e7e8173254a262144ea1a28
lolicon <ss.lbw.ss@gmail.com>
2022-07-01 15:41:06 +0800

    ✨ feat: component

---
+++ src/runtime-core/component.ts
@@ -0,0 +1,45 @@
+export function createComponentInstance(vNode: any) {
+  const component = {
+    vNode,
+    type: vNode.type,
+  }
+  return component
+}
+
+export function setupComponent(instance) {
+  // TODO:initProps
+  // TODO:initSlots
+
+  setupStatefulComponent(instance)
+}
+
+// 有状态的组件
+function setupStatefulComponent(instance) {
+  const Component = instance.type
+
+  const { setup } = Component
+
+  if (setup) {
+    // setup可以返回object或者function
+    const setupResult = setup()
+
+    handleSetupResult(instance, setupResult)
+  }
+}
+
+function handleSetupResult(instance, setupResult) {
+  // TODO:handle function
+
+  // handle object
+  if (typeof setupResult === 'object')
+    instance.setupState = setupResult
+
+  finishComponentSetup(instance)
+}
+
+function finishComponentSetup(instance: any) {
+  const Component = instance.type
+  if (Component.render)
+    instance.render = Component.render
+}
+
