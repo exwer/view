1c6aded31691a4847f6885a4bec6a980cdebd9f1
lolicon <ss.lbw.ss@gmail.com>
2022-07-01 16:10:42 +0800

    🔧 build: export createApp

--- src/runtime-core/index.ts
+++ src/runtime-core/index.ts
@@ -1,14 +1,1 @@
-import { render } from './renderer'
-import { createVNode } from './vNode'
-
-export function createApp(rootComponent) {
-  return {
-    mount(rootContainer) {
-      // 先转换成vNode
-      const vNode = createVNode(rootComponent)
-
-      render(vNode, rootContainer)
-    },
-  }
-}
-
+export { createApp } from './createApp'
